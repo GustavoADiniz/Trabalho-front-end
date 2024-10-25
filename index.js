dd1d3c7dfb564f8e2bf6d46b85ba2bbfd0c03772
GustavoADiniz <gustavodiniz135@gmail.com>
2024-10-25 19:38:06 -0300

    index.js

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 $(document).ready(function () {
-    $('#cep-message').text("").hide();
-    $('#company-name-span').hide();
+    $('#ddd-message').text("").hide();
+    $('#state-name-span').hide();
     $('#company-city-span').hide();
-    $('#company-place-span').hide();
-    $('#company-zip-span').hide();
 
-    $('#cep-value').on('input', function () {
+    $('#ddd-value').on('input', function () {
         const value = $(this).val();
-
         $(this).removeClass('border-danger').addClass('border-success'); // Adiciona a borda de sucesso
 
         if (!value) {
-            $('#cep-message').text("").hide();
-            $('#company-name-span').hide();
+            $('#ddd-message').text("").hide();
+            $('#state-name-span').hide();
             $('#company-city-span').hide();
-            $('#company-place-span').hide();
-            $('#company-zip-span').hide();
             $(this).removeClass('border-success border-danger');
         }
     });
 
-    $('#cep-form').on('submit', async function (event) {
+    $('#ddd-form').on('submit', async function (event) {
         event.preventDefault();
+        const ddd = $('#ddd-value').val().replace(/\D/g, ''); // Remove tudo que não é número
 
-        const cep = $('#cep-value').val().replace(/\D/g, ''); // Remove tudo que não é número
-
-        const cepData = await fetch(`https://brasilapi.com.br/api/cep/v1/${cep}`)
+        const dddData = await fetch(`https://brasilapi.com.br/api/ddd/v1/${ddd}`) // URL corrigida
             .then(response => {
                 if (!response.ok) {
                     return Promise.reject(response);
@@ -33,20 +27,19 @@ $(document).ready(function () {
                 return response.json();
             })
             .catch(error => {
-                // Limpa mensagens anteriores e define uma borda de erro
-                $('#cep-message').text("").hide();
-                $('#cep-value').removeClass('border-success').addClass('border-danger'); // Adiciona a borda de erro
+                $('#ddd-message').text("").hide();
+                $('#ddd-value').removeClass('border-success').addClass('border-danger'); // Adiciona a borda de erro
 
                 if (error.status === 404) {
                     Swal.fire({
                         title: "Erro",
-                        text: "CEP não foi encontrado",
+                        text: "DDD não foi encontrado",
                         icon: "error"
                     });
                 } else if (error.status === 400) {
                     Swal.fire({
                         title: "Erro",
-                        text: "O CEP foi digitado de maneira incorreta",
+                        text: "O DDD foi digitado de maneira incorreta",
                         icon: "error"
                     });
                 } else {
@@ -58,20 +51,14 @@ $(document).ready(function () {
                 }
             });
 
-        if (cepData) {
-            $('#cep-value').removeClass('border-danger').addClass('border-success'); // Adiciona a borda de sucesso
+        if (dddData) {
+            $('#ddd-value').removeClass('border-danger').addClass('border-success'); // Adiciona a borda de sucesso
 
-            $('#company-name-span').show();
-            $('#company-name').text(cepData.state);
+            $('#state-name-span').show();
+            $('#state-name').text(dddData.state);
 
             $('#company-city-span').show();
-            $('#company-city').text(cepData.city);
-
-            $('#company-place-span').show();
-            $('#company-place').text(cepData.neighborhood);
-
-            $('#company-zip-span').show();
-            $('#company-zip').text(cepData.street);
+            $('#company-city').text(dddData.cities.join(", "));
         }
     });
 });
